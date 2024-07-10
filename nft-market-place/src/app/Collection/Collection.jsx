c81cd04b78d7b62a7a28e00b1e68954ac55cfcde
3alehi <3alehi.ah@gmail.com>
2024-07-10 13:59:55 +0330

    response cole

--- nft-market-place/src/app/Collection/Collection.jsx
+++ nft-market-place/src/app/Collection/Collection.jsx
@@ -5,11 +5,13 @@ export default function Collection() {
   return (
     <div className="container">
       <div className={style.collection}>
+        <div className={style.header_col}>
         <h2>Trending Collection</h2>
         <p className={style.p_Box}>
           {" "}
           Checkout our weekly updated trending collection.
         </p>
+        </div>
         <div className={style.Box_div}>
 
         <Box />
