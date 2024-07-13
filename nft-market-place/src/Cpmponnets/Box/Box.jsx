43a348f548124d86d96186f0bc090aed9a6d6ee5
3alehi <3alehi.ah@gmail.com>
2024-07-13 15:22:24 +0330

    map header for nft collection

--- nft-market-place/src/Cpmponnets/Box/Box.jsx
+++ nft-market-place/src/Cpmponnets/Box/Box.jsx
@@ -1,25 +1,30 @@
 "use client";
 import React from "react";
 import style from "./Box.module.css";
-export default function Box() {
+export default function Box({poster,
+  imgColection1,
+  imgColection2,
+  creator,
+  desc,
+avatar}) {
   return (
     <div className={style.Box}>
       <img
         className={style.poster}
-        src="./img/Poster/Primary Photo Placeholder (1).png"
+        src={poster}
         alt=""
       />
       <div className={style.box_collection}>
-        <img src="./img/Poster/Secondary Photo Placeholder (1).png" alt="" />
-        <img src="./img/Poster/Secondary Photo Placeholder.png" alt="" />
+        <img src={imgColection1} alt="" />
+        <img src={imgColection2} alt="" />
         <div className={style.box_null}>
           <h2>+1254</h2>
         </div>
       </div>
-      <h3>DSGN Animals</h3>
+      <h3>{desc}</h3>
       <div className={style.collection_info}>
         <h3 className={style.Box_user_info}>
-          <img src="./img/Avatar/Avatar (4).png" alt="" /> MrFox
+          <img  className={ style.avatar}src={avatar} alt="" /> {creator}
         </h3>
       </div>
     </div>
--- nft-market-place/src/app/Collection/Collection.jsx
+++ nft-market-place/src/app/Collection/Collection.jsx
@@ -1,23 +1,67 @@
+"use client";
 import React from "react";
 import style from "./Collection.module.css";
 import Box from "@/Cpmponnets/Box/Box";
 export default function Collection() {
+  const Collection = [
+    {
+      id: 1,
+      Des: "DSGN Animals",
+      creator: "MrFox",
+      poster: "img/Poster/Primary Photo Placeholder (1).png",
+      imgColection: [
+        "img/Poster/Secondary Photo Placeholder (1).png",
+        "img/Poster/Secondary Photo Placeholder.png",
+      ],
+      Avatar:"img/Avatar/Avatar (4).png"
+
+    },
+    {
+      id: 2,
+      Des: "Magic Mushrooms",
+      creator: "Shroomie",
+      poster: "img/Poster/Primary Photo Placeholder (2).png",
+      imgColection: [
+        "img/Poster/Secondary Photo Placeholder (2).png",
+        "img/Poster/Primary Photo Placeholder (2).png",
+      ],
+      Avatar:"./img/Avatar/Avatar Placeholder (14).png"
+    },
+    {
+      id: 3,
+      Des: "Disco Machines",
+      creator: "BeKind2Robots",
+      poster: "img/Poster/Avatar Placeholder (15).png",
+      imgColection: [
+        "img/Poster/Secondary Photo Placeholder (5).png",
+        "img/Poster/Primary Photo Placeholder (3).png",
+      ],
+      Avatar:"./img/Avatar/Avatar Placeholder (13).png"
+
+    },
+  ];
+
   return (
     <div className="container">
       <div className={style.collection}>
         <div className={style.header_col}>
-        <h2>Trending Collection</h2>
-        <p className={style.p_Box}>
-          {" "}
-          Checkout our weekly updated trending collection.
-        </p>
+          <h2>Trending Collection</h2>
+          <p className={style.p_Box}>
+            {" "}
+            Checkout our weekly updated trending collection.
+          </p>
         </div>
         <div className={style.Box_div}>
-
-        <Box />
-        <Box />
-        <Box />
-        
+          {Collection.map((data) => (
+            <Box
+              poster={data.poster}
+              imgColection1={data.imgColection[0]}
+              imgColection2={data.imgColection[1]}
+              creator={data.creator}
+              desc={data.Des}
+              avatar={data.Avatar}
+            />
+          ))}
         </div>
       </div>
     </div>
