3b660ae2dc260e85301397fcb3807d0bd26416bc
3alehi <3alehi.ah@gmail.com>
2024-07-28 15:27:13 +0330

    responsive morenft

--- nft-market-place/src/Cpmponnets/MoreNftBox/MoreNftBox.jsx
+++ nft-market-place/src/Cpmponnets/MoreNftBox/MoreNftBox.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import style from "./MoreNftBox.module.css";
-export default function MoreNftBox() {
+export default function MoreNftBox({
+  name,
+  price,
+  hprice,
+  creator,
+  nftpic,
+  avatar,
+}) {
   return (
-      <div className={style.MoreNftBox}>
-        <div className={style.body}>
-          <img className={style.Img_nft} src="./img/nft/Image.jpg" alt="" />
-   <div style={{margin:"20px"}}>
-   <div >
-        <h2>AstroFiction</h2>
-        <div className={style.nft_info}>
-          <img
-            className={style.Avatar_info}
-            src="./img/Avatar/Avatar Placeholder (13).png"
-            alt=""
-          />
-          <span className={style.creator_nft}>Spaceone</span>
+    <div className={style.MoreNftBox}>
+      <div className={style.body}>
+        <img className={style.Img_nft} src={nftpic} alt="" />
+        <div style={{ margin: "20px" }}>
+          <div>
+            <h2>{name}</h2>
+            <div className={style.nft_info}>
+              <img
+                className={style.Avatar_info}
+                src={avatar}
+                alt=""
+              />
+              <span className={style.creator_nft}>{creator}</span>
+            </div>
+          </div>
+          <div className={style.price}>
+            <div className={style.price_Top}>
+              <p>Price</p>
+              <p>Highest Bid</p>
+            </div>
+            <div className={style.hight_Bottom}>
+              <p>{price} ETH</p>
+              <p>{hprice}wETH </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={style.price}>
-        <div className={style.price_Top}>
-          <p>Price</p>
-          <p>Highest Bid</p>
-        </div>
-        <div className={style.hight_Bottom}>
-          <p>1.63 ETH</p>
-          <p>0.33 wETH </p>
-        </div>
-      </div>
-   </div>
-    </div>
     </div>
   );
 }
